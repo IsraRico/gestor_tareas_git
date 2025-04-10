b19d230e71c5859b4a17e0b6a09bef1d8f47f842
salvador israel rico garcia <ricoisrael671@gmail.com>
2025-04-10 17:36:16 -0600

    Versión 2: Ver tareas

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# main.py
+
+
 print("¡Bienvenido al Gestor de Tareas!")
 
+tarea = input("Escribe una nueva tarea: ")
+with open("tareas.txt", "a") as archivo:
+    archivo.write(tarea + "\n")
+print("Tarea guardada.")
+
+print("\nTus tareas:")
+with open("tareas.txt", "r") as archivo:
+    print(archivo.read())
